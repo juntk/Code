5af59783dc8148cb8798a0b84dae333110070488
unknown <ats@.(none)>
2011-01-17 08:16:37 +0900

    fix

--- ruby/blog/Lagrange/Lagrange_Gen.rb
+++ ruby/blog/Lagrange/Lagrange_Gen.rb
@@ -3,28 +3,27 @@ require "Lagrange.rb"
 class Lagrange_Gen < Lagrange
 	def generate(x)
 		# mathematical expression
-		@a = []
-		@a_str = []
-		@array.each_with_index do |n,count|
-			@b = 1.0
-			@c = 1.0
-			@b_str = ''
-			@c_str = ''
-			@array.each_with_index do |n_,count_|
+		y_str = []
+		y = 0.0
+		@array.each_with_index do |i,count|
+			a = 1.0
+			b = 1.0
+			a_str = ''
+			b_str = ''
+			@array.each_with_index do |j,count_|
 				if count != count_ then
-					@b_str += '('+x.to_s+' - '+n_[0].to_s+')*'
-					@c_str += '('+n[0].to_s+' - '+n_[0].to_s+')*'
-					@b *= (x - n_[0])
-					@c *= (n[0] - n_[0])
+					a_str += '('+x.to_s+' - '+j[0].to_s+')*'
+					b_str += '('+i[0].to_s+' - '+j[0].to_s+')*'
+					a *= (x - j[0])
+					b *= (i[0] - j[0])
 				end
 			end
-			str = n[1].to_s + " * ((" + @b_str.chomp('*') + ') / (' + @c_str.chomp('*') +')) + '
-			num = (n[1] * (@b / @c))
-			@a_str << str
-			@a << num
+			str = i[1].to_s + " * ((" + a_str.chomp('*') + ') / (' + b_str.chomp('*') +')) + '
+			y += (i[1] * (a / b))
+			y_str << str
 		end
 		str = ''
-		@a_str.each do |s|
+		y_str.each do |s|
 			str += s
 		end
 		return str.chomp('+ ')
