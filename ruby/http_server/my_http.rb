69569904fc570b66788c5061f629d4412e0a7d06
juntk <jun.takeda.jp+github2@gmail.com>
2011-02-05 02:51:49 +0900

    fix

--- ruby/http_server/my_http.rb
+++ ruby/http_server/my_http.rb
@@ -25,61 +25,59 @@ EOS
 		while true
 			puts @gs
 			Thread.start(@gs.accept) do |s|
+                # 要求受け取り後、Parse
 				req = require_parser(s.gets)
                 p req
-                case req[:extension]
-                when 'bmp','jpg','jpeg','png','gif' then
-                    puts "request: image file"
-                    send_img(s,req[:path])
-                when 'html' then
-                    send_html(s,req[:path])
-                else
-                    puts "error"
+                # ファイル場所
+                path = @htdocs + req[:path]
+                puts path
+                # このスレッドで使うヘッダ情報
+                header = @http_header
+                begin
+                    # START 要求ファイル存在確認
+                    if File.exist?(path) then
+                        puts "exist"
+                        # START 拡張子判定
+                        # 米 拡張子ごとにメソッド作るのは止めた
+                        case req[:extension]
+                        when 'bmp','jpg','jpeg','png','gif' then
+                            puts "request: image file"
+                            header += "Content-Type: image/bmp\r\n"
+                            file = open(path,'rb')
+                         	data = file.read			
+                            file.close
+                        when 'html','txt' then
+                            file = open(path, 'r')
+	        	            data = file.read
+                        else
+                            # 米 該当する拡張子がなければバイナリ読込で（とりあえず）
+                            file = open(path, 'rb')
+                            data = file.read
+                        end
+                        # END 拡張子設定
+                        s.write(header+"Content-Length:"+data.length.to_s+"\r\n\r\n"+data)
+                        file.close
+                    elsif
+                        puts "not exist"
+                        send_404(s)
+                    end
+                    # END 要求ファイル存在確認
+                rescue => e
+                    p e
+                    #ここ内部エラーに
                     send_404(s)
                 end
-                logdump(req)
+                # Thread おわり
                 s.close
 			end
 		end
 	end
     def send_404(s)
-        s.write(@http_header_404)
-        puts "send 404"
-    end
-    def send_html(s,path)
         begin
-            path = @htdocs + path
-            puts path
-            if File.exist?(path) then
-                # 続きここから
-            end
-        rescue => e
-            p e
-        ensure
-            send_404(s)
-        end
-    end
-    def send_img(s,path)
-        # s:thread
-        begin
-        path = @htdocs + path
-        puts path
-        if File.exist?(path) then
-            puts "exist"
-	        file = open(path,"rb")
-            header = @http_header + "Content-Type: image/bmp\r\n"
-	    	data = file.read			
-    		s.write(header+"Content-Length:"+data.length.to_s+"\r\n\r\n"+data)
-            file.close
-        elsif
-            puts "not exist"
             s.write(@http_header_404)
-        end
+            puts "send 404"
         rescue => e
             p e
-        ensure
-            #ここ内部エラーに
-            send_404(s)
         end
     end
     def logdump(body)
